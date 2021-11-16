1f7c460ca9223e350426a71295c6378bc11590ed
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2021-11-17 00:06:23 +0200

    fix wait.sh to actually work

--- .tanzu/wait.sh
+++ .tanzu/wait.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
 workloadName=$1
-imageRef=$(kubectl get workload ${workloadName} -o=jsonpath='{.spec.source.image}')
-jsonpath='{.items[?(@.metadata.annotations.developer\.apps\.tanzu\.vmware\.com/image-source-digest=="'$imageRef'")].metadata.name}'
-while [[ -z $(kubectl get pod -o jsonpath=$jsonpath) ]]; do echo "Waiting for new workload \"${workloadName}\"" && sleep 10; done
-podName=$(kubectl get pod -o jsonpath=$jsonpath)
+jsonpath='{.items[0].metadata.name}'
+while [[ -z $(kubectl get pod --selector carto.run/workload-name=${workloadName},app.kubernetes.io/component=run 2>/dev/null) ]]; do echo "Waiting for new workload \"${workloadName}\"" && sleep 10; done
+podName=$(kubectl get pod --selector carto.run/workload-name=${workloadName},app.kubernetes.io/component=run -o jsonpath=$jsonpath)
 kubectl wait pod --for=condition=ready $podName
